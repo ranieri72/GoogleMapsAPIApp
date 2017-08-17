21056539061bf17df6c91dd3d79d2218fdfdf632
ranieri72 <ranieri72@hotmail.com>
2017-08-17 18:14:34 -0300

    Atualização da rota atual e comunicação com o Firebase

--- app/src/main/java/com/example/mtz_5555_transp/mymapapplication/MapsActivity.java
+++ app/src/main/java/com/example/mtz_5555_transp/mymapapplication/MapsActivity.java
@@ -12,6 +12,7 @@ import android.os.Parcelable;
 import android.os.PersistableBundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -27,8 +28,9 @@ import com.example.mtz_5555_transp.mymapapplication.Util.BuscarLocalTask;
 import com.example.mtz_5555_transp.mymapapplication.Util.MessageDialogFragment;
 import com.example.mtz_5555_transp.mymapapplication.Util.RotaTask;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,40 +44,48 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {//, LocationListener {
 
     static final int LOADER_ROTA = 2;
     static final int LOADER_ENDERECO = 1;
     static final String EXTRA_ORIG = "orig";
     static final String EXTRA_DEST = "dest";
     static final String EXTRA_ROTA = "rota";
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "location_updates";
 
     @BindView(R.id.edt_local)
     EditText mEdtLocal;
-
     @BindView(R.id.imgBtn_buscar)
     ImageButton mBtnBuscar;
-
     @BindView(R.id.txt_progresso)
     TextView mTxtProgresso;
-
     @BindView(R.id.llProgresso)
     LinearLayout mLayoutProgresso;
 
-    private Marker mMarkerLocalAtual;
+    // Firebase Database
+    private DatabaseReference mRoutesDatabaseReference;
+
     private ArrayList<LatLng> mRota;
-    private LoaderManager mLoaderManager;
     private LatLng mDestino;
     private GoogleMap mGoogleMap;
     private LatLng mOrigem;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+    private boolean mRequestingLocationUpdates;
+
+    private LoaderManager mLoaderManager;
     private BitmapDescriptor polylineOrigem;
     private BitmapDescriptor polylineDestino;
     LoaderManager.LoaderCallbacks<List<LatLng>> mRotaCallback = new LoaderManager.LoaderCallbacks<List<LatLng>>() {
@@ -92,9 +102,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 public void run() {
                     mRota = new ArrayList<>(data);
                     //Toast.makeText(MapsActivity.this, String.valueOf(data.get(data.size()-1).latitude), Toast.LENGTH_LONG).show();
-                    if(mRota.size() > 0){
-                        int i = mRota.size();
-                    }
                     atualizarMapa();
                     ocultarProgresso();
                 }
@@ -126,7 +133,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         public void onLoaderReset(Loader<List<Address>> loader) {
         }
     };
-    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,6 +144,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        updateValuesFromBundle(savedInstanceState);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                mRoutesDatabaseReference.push().setValue(locationResult.getLastLocation());
+                //mRoutesDatabaseReference.push().setValue(new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude()));
+
+//                for (Location location : locationResult.getLocations()) {
+//                }
+                Date date = new Date(locationResult.getLastLocation().getTime());
+                Toast.makeText(MapsActivity.this, locationResult.getLastLocation().getProvider()
+                        + " Data: " + date
+                        + " Speed: " + String.valueOf(((locationResult.getLastLocation().getSpeed() * 3600) / 1000)), Toast.LENGTH_SHORT).show();
+            }
+        };
 
         mLoaderManager = getSupportLoaderManager();
 
@@ -145,6 +167,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .fromResource(R.drawable.ic_polyline_destino);
         polylineOrigem = BitmapDescriptorFactory
                 .fromResource(R.drawable.ic_polyline_origem);
+
+        mRoutesDatabaseReference = FirebaseDatabase.getInstance().getReference().child("routes");
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        // Update the value of mRequestingLocationUpdates from the Bundle.
+        if (savedInstanceState != null && savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+            mRequestingLocationUpdates = savedInstanceState.getBoolean(
+                    REQUESTING_LOCATION_UPDATES_KEY);
+        }
     }
 
     @Override
@@ -153,6 +185,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         outState.putParcelable(EXTRA_ORIG, mOrigem);
         outState.putParcelable(EXTRA_DEST, mDestino);
         outState.putParcelable(EXTRA_ROTA, (Parcelable) mRota);
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
     }
 
     @Override
@@ -166,6 +200,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         if (estaCarregando(LOADER_ENDERECO) && mDestino == null) {
@@ -175,19 +223,61 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             mLoaderManager.initLoader(LOADER_ROTA, null, mRotaCallback);
             exibirProgresso("Carregando rota...");
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        getLastLocation();
+    }
 
-            return;
+    private void getLastLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                dialogPermission();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }
+        } else {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        mOrigem = new LatLng(location.getLatitude(), location.getLongitude());
+                        atualizarMapa();
+                    }
+                }
+            });
         }
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    mOrigem = new LatLng(location.getLatitude(), location.getLongitude());
-                    atualizarMapa();
+    }
+
+    private void dialogPermission() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissão de GPS");
+        builder.setMessage("Esse aplicativo necessita da permissão de uso do GPS para funcionar");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+                if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                } else {
+                    getLastLocation();
                 }
             }
         });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        AlertDialog alerta = builder.create();
+        alerta.show();
     }
 
     private boolean estaCarregando(int loaderEndereco) {
@@ -258,16 +348,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         exibirProgresso("Procurando endereço...");
     }
 
-    private void iniciarDeteccaoDeLocal() {
+    private void startLocationUpdates() {
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(5 * 1000);
         locationRequest.setFastestInterval(1000);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //return;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(locationRequest,
+                    mLocationCallback,
+                    null /* Looper */);
         }
-        //mFusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback(), new Looper());
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
 
     private void atualizarMapa() {
@@ -283,12 +380,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             mGoogleMap.addMarker(new MarkerOptions().position(mDestino).title("Destino").icon(polylineDestino));
             mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(area, 50));
 
-            mMarkerLocalAtual = mGoogleMap.addMarker(new MarkerOptions()
+            Marker mMarkerLocalAtual = mGoogleMap.addMarker(new MarkerOptions()
                     .position(mDestino)
                     .title("Destino")
                     .icon(polylineOrigem)
                     .position(mOrigem));
-            iniciarDeteccaoDeLocal();
+            startLocationUpdates();
 
             //Toast.makeText(MapsActivity.this, String.valueOf(mRota.get(mRota.size() - 1).latitude), Toast.LENGTH_LONG).show();
 
@@ -300,7 +397,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             mGoogleMap.addPolyline(polylineOptions);
         } else {
             if (mOrigem != null) {
-                //Toast.makeText(MapsActivity.this, "Atualizando mapa sem rota!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MapsActivity.this, "Atualizando mapa sem rota!", Toast.LENGTH_SHORT).show();
                 mGoogleMap.addMarker(new MarkerOptions().position(mOrigem).title("Local Atual"));
                 mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mOrigem, 17.0F));
             }
@@ -317,9 +414,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Toast.makeText(MapsActivity.this, "Localização alterada!", Toast.LENGTH_SHORT).show();
-        mMarkerLocalAtual.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
-    }
+//    @Override
+//    public void onLocationChanged(Location location) {
+//        Toast.makeText(MapsActivity.this, "Localização alterada!", Toast.LENGTH_SHORT).show();
+//        mMarkerLocalAtual.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+//    }
 }
